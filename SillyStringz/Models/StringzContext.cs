a3a58b838dd98895fcd8de20ea5aa416f9529972
Christopher Quattrocchi <chrisquattrocchi@gmail.com>
2024-02-12 07:39:55 -0800

    ref code for context?

--- SillyStringz/Models/StringzContext.cs
+++ SillyStringz/Models/StringzContext.cs
@@ -14,3 +14,35 @@ namespace Stringz.Models
     public FactoryContext(DbContextOptions options) : base (options) { }
   }
 }
+
+
+//confusing chatgpt code below for reference
+// using Microsoft.EntityFrameworkCore;
+
+// namespace Factory.Models
+// {
+//     public class FactoryContext : DbContext
+//     {
+//         public FactoryContext(DbContextOptions<FactoryContext> options) : base(options) {}
+
+//         protected override void OnModelCreating(ModelBuilder builder)
+//         {
+//             builder.Entity<MachineEngineer>()
+//                 .HasKey(me => new { me.EngineerId, me.MachineId });
+
+//             builder.Entity<MachineEngineer>()
+//                 .HasOne(me => me.Engineer)
+//                 .WithMany(e => e.Machines)
+//                 .HasForeignKey(me => me.EngineerId);
+
+//             builder.Entity<MachineEngineer>()
+//                 .HasOne(me => me.Machine)
+//                 .WithMany(m => m.Engineers)
+//                 .HasForeignKey(me => me.MachineId);
+//         }
+
+//         public DbSet<Engineer> Engineers { get; set; }
+//         public DbSet<Machine> Machines { get; set; }
+//         public DbSet<MachineEngineer> MachineEngineers { get; set; }
+//     }
+// }
